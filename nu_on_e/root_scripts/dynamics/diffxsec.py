e0add9e41c07489db0fe78abf91cbaa62c5c2946
kaikai <kaikai5816787@gmail.com>
2015-09-03 12:07:28 -0600

    angular cut efficiency with python

--- nu_on_e/root_scripts/dynamics/diffxsec.py
+++ nu_on_e/root_scripts/dynamics/diffxsec.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
+from scipy.interpolate import interp1d
 from math import *
 
 def diffxsec(x):
@@ -44,13 +45,27 @@ plt.plot(t1, y)
 fig2 = plt.figure()
 fig2.patch.set_facecolor('white')
 ax1 = fig2.add_subplot(211)
+plt.yscale('log', nonposy='clip')
+plt.xlabel(r'$\cos\theta$', fontsize=16, labelpad=0)
+plt.ylabel(r'$\frac{d\sigma}{d\cos\theta}/\sigma_0$', fontsize=16)
+plt.plot(t1, y, linewidth=2.0)
+
 ax2 = fig2.add_subplot(212)
 x2 = np.arange(0., 90., 0.001)
 totxsec = quad(diffxsec, 0, 1)
 y2 = angeff(x2)
 yeff = [x/totxsec[0] for x in y2[0]]
-plt.xlabel(r'$\theta$', fontsize=16)
+plt.xlabel(r'$\theta$ (degree)', fontsize=16)
 plt.ylabel(r'$\epsilon_\theta$', fontsize=16)
-plt.plot(x2, yeff)
+plt.plot(x2, yeff, 'r', linewidth=2.0)
+plt.savefig('plots/xsec_cut_eff.eps')
+
+# caculate the angles corresponding to 90% and 99% efficiencies
+# note that show() will lock the terminal, therefore have to put code in front
+eff_interp = interp1d(yeff, x2)
+f = open('efficiency_angle.txt', 'w')
+f.write('angle of 90% efficiency: ' + str(eff_interp(.9)) + '\n')
+f.write('angle of 99% efficiency: ' + str(eff_interp(.99)))
+f.close()
 
 plt.show()
