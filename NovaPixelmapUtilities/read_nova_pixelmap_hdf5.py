90825202af349b72710071d1da6607a31b8bd856
kaikai <kaikai581@hotmail.com>
2017-11-18 20:04:59 -0600

    Ground truth labels added.

--- NovaPixelmapUtilities/read_nova_pixelmap_hdf5.py
+++ NovaPixelmapUtilities/read_nova_pixelmap_hdf5.py
@@ -21,15 +21,48 @@ entry = args.entry
 ch = args.channel
 
 dataset = inf['/data']
+label = inf['/label']
+
+predefined_labes = []
+predefined_labes.append('kNumuQE')
+predefined_labes.append('kNumubarQE')
+predefined_labes.append('kNumuRes')
+predefined_labes.append('kNumubarRes')
+predefined_labes.append('kNumuDIS')
+predefined_labes.append('kNumubarDIS')
+predefined_labes.append('kNumuOther')
+predefined_labes.append('kNumubarOther')
+predefined_labes.append('kNueQE')
+predefined_labes.append('kNuebarQE')
+predefined_labes.append('kNueRes')
+predefined_labes.append('kNuebarRes')
+predefined_labes.append('kNueDIS')
+predefined_labes.append('kNuebarDIS')
+predefined_labes.append('kNueOther')
+predefined_labes.append('kNuebarOther')
+predefined_labes.append('kNutauQE')
+predefined_labes.append('kNutaubarQE')
+predefined_labes.append('kNutauRes')
+predefined_labes.append('kNutaubarRes')
+predefined_labes.append('kNutauDIS')
+predefined_labes.append('kNutaubarDIS')
+predefined_labes.append('kNutauOther')
+predefined_labes.append('kNutaubarOther')
+predefined_labes.append('kNuElectronElastic')
+predefined_labes.append('kNC')
+predefined_labes.append('kCosmic')
+predefined_labes.append('kOther')
+predefined_labes.append('kNIntType')
 
 print('Reading data...')
 data_read = dataset[...]
+label_read= label[...]
 print('Printing data...')
 
 H = np.array(data_read[entry][ch])
 fig = plt.figure(figsize=(6, 3.2))
 ax = fig.add_subplot(111)
-ax.set_title('colorMap')
+ax.set_title(predefined_labes[label_read[entry][0][0][0]])
 plt.imshow(H)
 ax.set_aspect('equal')
 
